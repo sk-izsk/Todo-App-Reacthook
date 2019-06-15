4f592bf457675bca350d9847b9b305f7a1f27f70
sk-izsk <sk.zeeshan1992@gmail.com>
2019-06-14 21:14:57 -0400

    Complete todo app in one file also add extra func

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,22 @@ import React from "react";
 import { Component } from "react";
 import { useState } from "react";
 
-function Todo({ todo, index }) {
-  return <div className="todo">{todo.text}</div>;
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+      className="todo"
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>
+          {" "}
+          {todo.isCompleted ? "Not Complete ?" : "Complete"}{" "}
+        </button>
+        <button onClick={() => removeTodo(index)}> X </button>
+      </div>
+    </div>
+  );
 }
 
 function TodoForm({ addTodo }) {
@@ -22,6 +36,7 @@ function TodoForm({ addTodo }) {
         className="input"
         value={value}
         onChange={e => setValue(e.target.value)}
+        placeholder="Add your todo"
       />
     </form>
   );
@@ -47,11 +62,27 @@ function App() {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
   };
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
+  };
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>

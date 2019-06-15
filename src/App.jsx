ffda6ae5954619a9d2f8f9e93ee7325cc320f6f1
sk-izsk <sk.zeeshan1992@gmail.com>
2019-06-14 21:00:05 -0400

    Add Css and implement basic todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import { Component } from "react";
+import { useState } from "react";
 
-export default class App extends Component {
-  render() {
-    return <div>hello</div>;
-  }
+function Todo({ todo, index }) {
+  return <div className="todo">{todo.text}</div>;
 }
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "learn about React",
+      isCompleted: false
+    },
+    {
+      text: "meet friend",
+      isCompleted: false
+    },
+    {
+      text: "build app",
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+export default App;
